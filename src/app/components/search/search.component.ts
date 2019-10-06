539fc9264c486dcf0adafb05b05a156c1f7c262d
Edwin Vivas <edwinvivassoto@gmail.com>
2019-10-06 16:08:59 -0500

    spotyfy con mejoras

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styles: []
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
 
-  constructor() { }
+  artistas: any[] = [];
 
-  ngOnInit() {
-  }
+  constructor( private spotify: SpotifyService ) { }
+
+buscar (termino: string) {
+console.log(termino);
+this.spotify.getArtista( termino )
+.subscribe( (data:any) => {
+console.log(data);
+this.artistas = data;
+
+});
+
+}
+
 
 }
